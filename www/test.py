1e342f146ae458cb9c8ae2f01d7d04a1fa7a9e7f
David <zxczhen2007@126.com>
2017-02-20 14:14:53 +0800

    test for mysql day4

---
+++ www/test.py
@@ -0,0 +1,18 @@
+import orm,asyncio,sys
+from models import User, Blog, Comment
+
+@asyncio.coroutine
+def test(loop):
+    yield from orm.create_pool(loop=loop,user='www-data', password='www-data', db='awesome')
+
+    u=User(name='test11',email='test11@test.com',passwd='test',image='about:blank')
+
+    yield from u.save()
+
+if __name__ == '__main__':
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete( asyncio.wait([test( loop )]) )  
+    loop.close()
+    if loop.is_closed():
+        sys.exit(0)
